4b8cfb5af3344e955a285dc8f40c4102e80dbc86
Fennec82 <NOTPUTTINGTHISHERE@TURBONERD.COM>
2024-04-17 12:12:21 -0400

    Fixes capitalization errors

--- RNGspinner/spinner.py
+++ RNGspinner/spinner.py
@@ -2,17 +2,17 @@ import random
 
 reward_probs = { 
         'Chord Mojo 2': 0.00001,
-        'nothing': 0.5,
+        'Nothing': 0.5,
         '30 minutes of video games': 0.05, 
         '30  mins of phone time': 0.1, 
         'SS13 with friends on weekend': 0.05, 
-        'eat takeout': 0.05, 
+        'Get takeout': 0.05, 
         '20 mins of anything': 0.05, 
         '20 mins of model building': 0.05, 
-        'Trip to Newbury Comics': 0.05, 
+        'Trip to Newbury Comics': 0.04, 
         'Trip to Games Workshop': 0.05, 
-        'read an news article': 0.1, 
-        'two spins': 0.04999, 
+        'Read a news article': 0.1, 
+        'Two spins': 0.04999, 
 }
 def spin(): 
     return random.choices(list (reward_probs. keys ()), list (reward_probs. values () )) [0]
